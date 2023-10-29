f761fcfad2ddad837a904e91fee714cda1ee1c5f
Bernard-Otieno <bernard.otieno@strathmore.edu>
2023-10-29 13:46:18 +0300

    done

--- Lab7-Submission-AlgorithmSelection.R
+++ Lab7-Submission-AlgorithmSelection.R
@@ -95,9 +95,9 @@ if (require("rpart")) {
 #### Load and split the dataset ----
 data(Glass)
 
-# Define a 70:30 train:test data split of the dataset.
+# Define a 80:20 train:test data split of the dataset.
 train_index <- createDataPartition(Glass$Type,
-                                   p = 0.7,
+                                   p = 0.8,
                                    list = FALSE)
 Glass_train <- Glass[train_index, ]
 Glass_test <- Glass[-train_index, ]
@@ -120,65 +120,6 @@ confusion_matrix <-
   caret::confusionMatrix(predictions,
                         Glass_test[, 1:10]$Type)
 print(confusion_matrix)
-### 1.b. Decision tree for a regression problem without CARET ----
-#### Load and split the dataset ----
-data(Glass)
-
-# Define an 80:20 train:test data split of the dataset.
-train_index <- createDataPartition(Glass$Type,
-                                   p = 0.8,
-                                   list = FALSE)
-boston_housing_train <-Glass[train_index, ]
-boston_housing_test <-Glass[-train_index, ]
-
-#### Train the model ----
-Glass_model_cart <- rpart(Type ~ ., data =Glass_train,
-                            control = rpart.control(minsplit=5))
-
-#### Display the model's details ----
-print(Glass_model_cart)
-
-#### Make predictions ----
-predictions <- predict(Glass_model_cart,Glass_test[, 1:9])
-
-#### Display the model's evaluation metrics ----
-##### RMSE ----
-if (is.factor(Glass_test$Type)) {
-  Glass_test$Type <- as.numeric(as.character(Glass_test$Type))
-}
-rmse <- sqrt(mean((Glass_test$Type-predictions)^2))
-print(paste("RMSE =", sprintf(rmse, fmt = "%#.4f")))
-
-##### SSR ----
-# SSR is the sum of squared residuals (the sum of squared differences
-# between observed and predicted values)
-if (is.factor(Glass_test$Type)) {
-  Glass_test$Type <- as.numeric(as.character(Glass_test$Type))
-}
-ssr <- sum((Glass_model_cart$Type - predictions)^2)
-print(paste("SSR =", sprintf(ssr, fmt = "%#.4f")))
-
-##### SST ----
-# SST is the total sum of squares (the sum of squared differences
-# between observed values and their mean)
-Glass_model_cart$Type <- as.numeric(as.character(Glass_model_cart$Type))
-sst <- sum((Glass_model_cart$Type - mean(Glass_model_cart$Type))^2)
-print(paste("SST =", sprintf(sst, fmt = "%#.4f")))
-
-##### R Squared ----
-# We then use SSR and SST to compute the value of R squared.
-# The closer the R squared value is to 1, the better the model.
-r_squared <- 1 - (ssr / sst)
-print(paste("R Squared =", sprintf(r_squared, fmt = "%#.4f")))
-
-##### MAE ----
-# MAE is expressed in the same units as the target variable, making it easy to
-# interpret. For example, if you are predicting the amount paid in rent,
-# and the MAE is KES. 10,000, it means, on average, your model's predictions
-# are off by about KES. 10,000.
-absolute_errors <- abs(predictions -Glass_test$Type)
-mae <- mean(absolute_errors)
-print(paste("MAE =", sprintf(mae, fmt = "%#.4f")))
 
 ### 2.a. Naïve Bayes Classifier for a Classification Problem without CARET ----
 # We use the naiveBayes function inside the e1071 package
@@ -201,6 +142,106 @@ print(Glass_model_nb)
 
 #### Make predictions ----
 predictions <- predict(Glass_model_nb,
+                       Glass_test[, -10])
+
+#### Display the model's evaluation metrics ----
+confusion_matrix <-
+  caret::confusionMatrix(predictions,
+                         Glass_test[, 1:10]$Type)
+print(confusion_matrix)
+
+### 2.b. Naïve Bayes Classifier for a Classification Problem with CARET ----
+#### Load and split the dataset ----
+data(Glass)
+
+# Define a 70:30 train:test data split of the dataset.
+train_index <- createDataPartition(Glass$Type,
+                                   p = 0.7,
+                                   list = FALSE)
+Glass_train <- Glass[train_index, ]
+Glass_test <- Glass[-train_index, ]
+
+#### Train the model ----
+# We apply the 5-fold cross validation resampling method
+set.seed(7)
+train_control <- trainControl(method = "cv", number = 10)
+preProcessObj <- preProcess(Glass_train, method = "medianImpute")
+Glass_train_imputed <- predict(preProcessObj, Glass_train)
+Glass_caret_model_nb <- train(Type ~ .,
+                                 data = Glass_train_imputed,
+                                 method = "nb", metric = "Accuracy",
+                                 trControl = train_control)
+
+#### Display the model's details ----
+print(Glass_caret_model_nb)
+
+#### Make predictions ----
+predictions <- predict(Glass_caret_model_nb,
+                       Glass_train_imputed[, 1:9])
+
+#### Display the model's evaluation metrics ----
+confusion_matrix <-
+  caret::confusionMatrix(predictions,
+                         Glass_train_imputed[, 1:10]$Type)
+print(confusion_matrix)
+
+### 3.a. kNN for a classification problem without CARET's train function ----
+#### Load and split the dataset ----
+data(Glass)
+
+# Define a 70:30 train:test data split of the dataset.
+train_index <- createDataPartition(Glass$Type,
+                                   p = 0.8,
+                                   list = FALSE)
+Glass_train <- Glass[train_index, ]
+Glass_test <- Glass[-train_index, ]
+
+#### Train the model ----
+Glass_model_knn <- knn3(Type ~ ., data = Glass_train, k=2)
+
+#### Display the model's details ----
+print(Glass_model_knn)
+
+#### Make predictions ----
+predictions <- predict(Glass_model_knn,
+                       Glass_test[, 1:9],
+                       type = "class")
+
+#### Display the model's evaluation metrics ----
+table(predictions, Glass_test$Type)
+
+# Or alternatively:
+confusion_matrix <-
+  caret::confusionMatrix(predictions,
+                         Glass_test$Type)
+print(confusion_matrix)
+
+
+### 3.c. kNN for a classification problem with CARET's train function ----
+#### Load and split the dataset ----
+data(Glass)
+
+# Define a 70:30 train:test data split of the dataset.
+train_index <- createDataPartition(Glass$Type,
+                                   p = 0.7,
+                                   list = FALSE)
+Glass_train <- Glass[train_index, ]
+Glass_test <- Glass[-train_index, ]
+
+#### Train the model ----
+
+set.seed(121)
+train_control <- trainControl(method = "cv", number = 10)
+Glass_caret_model_knn <- train(Type ~ ., data = Glass,
+                                  method = "knn", metric = "Accuracy",
+                                  preProcess = c("center", "scale"),
+                                  trControl = train_control)
+
+#### Display the model's details ----
+print(Glass_caret_model_knn)
+
+#### Make predictions ----
+predictions <- predict(Glass_caret_model_knn,
                        Glass_test[, 1:9])
 
 #### Display the model's evaluation metrics ----
@@ -209,7 +250,39 @@ confusion_matrix <-
                          Glass_test[, 1:10]$Type)
 print(confusion_matrix)
 
-fourfoldplot(as.table(confusion_matrix), color = c("grey", "lightblue"),
-             main = "Confusion Matrix")
+## 4.c. SVM Classifier for a classification problem with CARET ----
+  
+  #### Load and split the dataset ----
+data(Glass)
+
+# Define a 70:30 train:test data split of the dataset.
+train_index <- createDataPartition(Glass$Type,
+                                   p = 0.7,
+                                   list = FALSE)
+Glass_train <- Glass[train_index, ]
+Glass_test <- Glass[-train_index, ]
+
+#### Train the model ----
+set.seed(121)
+train_control <- trainControl(method = "cv", number = 11)
+Glass_caret_model_svm_radial <- # nolint: object_length_linter.
+  train(Type ~ ., data = Glass_train, method = "svmRadial",
+        metric = "Accuracy", trControl = train_control)
+
+#### Display the model's details ----
+print(Glass_caret_model_svm_radial)
+
+#### Make predictions ----
+predictions <- predict(Glass_caret_model_svm_radial,
+                       Glass_test[, 1:9])
+
+#### Display the model's evaluation metrics ----
+table(predictions, Glass_test$Type)
+confusion_matrix <-
+  caret::confusionMatrix(predictions,
+                         Glass_test[, 1:10]$Type)
+print(confusion_matrix)
+
+
 
 
